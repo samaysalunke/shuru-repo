3babe381133a3cb2afb257997b7bf45fd952b820
Samay Salunke <samaysalunke@gmail.com>
2025-10-19 22:40:21 +0530

    Fix langchain.chains import error for LangChain 0.2.0+
    - Updated ConversationalRetrievalChain import to use full module path
    - Changed from 'langchain.chains' to 'langchain.chains.conversational_retrieval.base'
    - Resolves ModuleNotFoundError: No module named 'langchain.chains'

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_anthropic import ChatAnthropic
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain_core.documents import Document
 from langchain_core.prompts import PromptTemplate
